4e3d8d236872ab3943bd82f016343aa5a6ebba01
Carolyn L. Phillips <cphillips@anl.gov>
2016-02-09 12:53:03 -0600

    added some distance metrics

--- feature_extraction/fourierdescriptors.py
+++ feature_extraction/fourierdescriptors.py
@@ -7,7 +7,49 @@
 import numpy as np
 import coordinate_helper
 
+
+#
+# Pad the shape descriptor out to a new length with zeros
+#
+def pad_sd(sd, new_length):
+
+    if new_length > len(sd):
+        tmp = np.zeros(new_length)
+        tmp[:len(sd)]=sd
+        sd = tmp
+    
+    return sd
+
+#
+# Calculating a normalized distance as is done in SMAC
+#
+def distance_func(sd1, sd2):
+
+    # Just in case the two shape descriptors are not the same length, pad with zeros
+    if len(sd1) != len(sd2):
+        new_length = np.max([len(sd1),len(sd2)])
+        sd1 = pad_sd(sd1,new_length)
+        sd2 = pad_sd(sd2,new_length)
+
+    max_difference = np.linalg.norm(sd1) + np.linalg.norm(sd2)
+    residual = np.linalg.norm(sd1-sd2)
+
+    rnorm = residual/max_difference
+
+    return rnorm
+
+#
+# Takes the normalized distance and returns the similarity instead
+# This is what is returned by matchfundist in SMAC.
+#
+def similarity_func(sd1,sd2,min=0,max=1.0):
+    rnorm = distance_func(sd1,sd2)
+    return min + (max-min)*(1-rnorm)
+
+
+#
 # Calculating the Invariant 2D Fourier Descriptor (weights are 1.0)
+#
 def fourier2D(coordinates,L,frequencies):
 
 
