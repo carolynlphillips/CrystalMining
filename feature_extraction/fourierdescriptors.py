ae7ec0fddfa03b70538d9587b482754aea234f71
Carolyn L. Phillips <cphillips@anl.gov>
2016-02-26 05:31:03 -0600

    added 3d Fourier Descriptor with invariance.  Not tested

--- feature_extraction/fourierdescriptors.py
+++ feature_extraction/fourierdescriptors.py
@@ -5,6 +5,7 @@
 #   based on passing polygons to descriptor
 
 import numpy as np
+import scipy.special as sp
 import coordinate_helper
 
 
@@ -101,4 +102,66 @@ def fourier2D(coordinates,L,frequencies):
         shape_descriptor.append(c)
 
     return shape_descriptor
+
+
+#
+# Calculating the Invariant 3D Fourier Descriptor (weights are 1.0)
+#
+def fourier3D(coordinates,L,frequencies):
+
+
+    # Note, that I do not perform the calculation below on the central particle
+    # Messes with results!
+    
+    N = coordinates.shape[0]-1;
+    
+    if N == 0:
+        return [0]
+    
+    # Note.  The first coordinates is the center particle.
+    #Translate all coordinates so the center is the origin
+    coordinates = coordinate_helper.pbc(coordinates[1:,:]-coordinates[0,:],L)
+
+    #-- STEP 1 - Convert to spherical  coordinates
+    # calculate the radial distance of all the coordinates
+    r = np.linalg.norm(coordinates)                            #Radial
+    phi = np.arctan2(coordinates[:,1],coordinates[:,0]) +np.pi #Azimuthal
+    theta = np.arccos(coordinates[:,2]/r)                      #Inclination
+
+    #-- STEP 2: compute Ylm(theta, phi) for each theta, phi
+
+    # Using spherical harmonic function provided by scipy
+    #http://docs.scipy.org/doc/scipy-0.16.0/reference/generated/scipy.special.sph_harm.html
+
+    # scipy.special.sph_harm(m, l, azimuthal, polar) azimuthal in [0,2pi], polar in [0,pi]  (n == l)
+
+    # NEED MORE WORK BELOW
+
+    # In the original smac code, for a given frequency l, loops over the n particles, and over m values from -l to +l, and then applies a "q" invarinace that combines all the m values generated for all the particles.
+
+    shape_descriptor= []
+    for l in frequencies:
+
+        components = np.zeros(shape=(coordinates.shape[0],2*l+1,2))
+        
+        for m in range(-l,l+1):
+            w = 1.0
+            ylm = sp.sph_harm(m,l,phi,theta)
+            components[:,m,0] = w*ylm.real
+            components[:,m,1] = w*ylm.imag
+
+
+        # Summing over all the particles
+        ylm_average = np.sum(components,axis=0)/components.shape[0]
+
+        # Applying invariance
+        # computes the "q" invariant for a 3d fourier coefficient
+        sd = np.sqrt(4.0*np.pi/ylm.shape[1]*np.sum(ylm_average**2))
+        shape_descriptor.append(sd)
+
+
+    return shape_descriptor
+
+
+
     
